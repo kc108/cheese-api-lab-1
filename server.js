b4190d1821ee1829e47214758a5de9357d2d2bbb
Kim Carpico <kcarpico@gmail.com>
2021-05-31 10:29:31 -0700

    corrected handleSubmit for the editForm route was missing 2nd param of cheese._id git push

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const express = require("express");
 const app = express();
 // import mongoose
 const mongoose = require("mongoose");
-// import middlware
+// import middleware
 const cors = require("cors");
 const morgan = require("morgan");
 
